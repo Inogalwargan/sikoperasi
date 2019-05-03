54081721f619e0956106e61f1380c90b513e9d34
ino <marianogalwargan@gmail.com>
2019-05-03 21:29:49 +0700

    Initial commit

---
+++ application/views/admin/_includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="main-footer">
+ 	<div class="pull-right hidden-xs">
+ 		<b>Version</b> 1.0.0
+ 	</div>
+ 	<strong>Copyright &copy; 2019 <a href="https://sisfocup2018.web.id">Ino Galwargan</a>.</strong> All rights
+ 	reserved.
+ </footer>
---
+++ application/views/admin/_includes/head.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/Ionicons/css/ionicons.min.css'); ?>" rel="stylesheet">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/dist/css/AdminLTE.min.css'); ?>"  rel="stylesheet">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/dist/css/skins/_all-skins.min.css'); ?>" rel="stylesheet">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/morris.js/morris.css'); ?>" rel="stylesheet">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/jvectormap/jquery-jvectormap.css'); ?>" rel="stylesheet">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'); ?>" rel="stylesheet">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/bower_components/bootstrap-daterangepicker/daterangepicker.css'); ?>" rel="stylesheet">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="<?php echo base_url('assetAdmin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'); ?>" rel="stylesheet">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
